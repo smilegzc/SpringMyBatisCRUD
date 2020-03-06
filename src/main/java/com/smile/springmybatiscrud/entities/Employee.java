fe2a5e536e3dce27cc830298c0bf11bc4bb5170b
smile <qq115051@outlook.com>
2020-03-06 13:08:53 +0800

    加入lombok简化实体类代码

--- src/main/java/com/smile/springmybatiscrud/entities/Employee.java
+++ src/main/java/com/smile/springmybatiscrud/entities/Employee.java
@@ -1,76 +1,13 @@
 package com.smile.springmybatiscrud.entities;
 
-import com.smile.springmybatiscrud.mapper.DepartmentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
 
+@Data
 public class Employee {
-
     private Integer id;
     private String lastName;
-
     private String email;
-    //1 male, 0 female
     private Integer gender;
     private String birth;
     private Department department;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getGender() {
-        return gender;
-    }
-
-    public void setGender(Integer gender) {
-        this.gender = gender;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public void setBirth(String birth) {
-        this.birth = birth;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                ", id=" + id +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", gender=" + gender +
-                ", birth='" + birth + '\'' +
-                ", dept=" + department +
-                '}';
-    }
 }
